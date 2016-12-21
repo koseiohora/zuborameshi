f8c2ba6bb1027e2aebf5dbdf1b37f208c5bd2793
kosei ohora <naiteitorio@gmail.com>
2016-12-21 14:04:42 +0900

    likesテーブルの作成 likes_countカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217150254) do
+ActiveRecord::Schema.define(version: 20161221050119) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -21,13 +21,21 @@ ActiveRecord::Schema.define(version: 20161217150254) do
     t.datetime "updated_at"
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string   "title",      limit: 191
-    t.string   "image",      limit: 191
-    t.text     "text",       limit: 65535
+  create_table "likes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "recipe_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "title",       limit: 191
+    t.string   "image",       limit: 191
+    t.text     "text",        limit: 65535
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "likes_count", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
