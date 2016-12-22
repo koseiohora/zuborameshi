d30a001a79a292395214e0b8f42ffe3cb7da2e41
kosei ohora <naiteitorio@gmail.com>
2016-12-22 18:58:01 +0900

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #get 'recipes/:id/edit' => 'recipes#edit'
   #get 'users/:id' => 'users#show'
   #get 'recipes/:id' => 'recipes
-  # get 'recipes/about' => 'recipes#about'
+  get 'recipes/about' => 'recipes#about'
   resources :recipes do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy], shallow: true
